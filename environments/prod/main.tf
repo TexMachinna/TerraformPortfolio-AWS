b0221ba64bd59a991825bb975492c50ce1a253ca
TexMachinna <texmachinna@DESKTOP-1Q89JFU.localdomain>
2026-07-22 11:25:14 -0600

    Added feature: Copied and adjusted Terraform Environment to Production / Added group_vars for Ansible and adjusted their .ini files accordingly

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -33,6 +33,7 @@ module "compute" {
   asg_desired_size = var.asg_desired_size
   asg_max_size     = var.asg_max_size
   asg_min_size     = var.asg_min_size
+  key_name         = var.key_name
 
   subnet_id         = module.networking.public_subnet_id
   security_group_id = module.security.security_group_id
--- environments/prod/variables.tf
+++ environments/prod/variables.tf
@@ -29,7 +29,7 @@ variable "private_subnet_cidr" {
 variable "cidr_ipv4" {
   description = "IPv4 blocks allowed to SSH the resources"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = ""
 }
 
 # Compute variables
@@ -50,6 +50,11 @@ variable "asg_max_size" {
 variable "asg_desired_size" {
   description = "Desired size for the ASG"
   type        = number
+}
+
+variable "key_name" {
+  description = "EC2 Key Pair name"
+  type        = string
 }
 
 # Monitoring variables
